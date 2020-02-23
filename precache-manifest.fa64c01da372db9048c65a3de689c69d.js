9953ca69d0e5f0f3e0e3d4c7063d7b81baddb621
Pete Redhead <pete.redhead@lobsterpictures.tv>
2020-02-23 21:17:10 +1300

    Updates

---
+++ precache-manifest.fa64c01da372db9048c65a3de689c69d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1ffa53028728a1f038e42ef40516a23",
+    "url": "/binder/index.html"
+  },
+  {
+    "revision": "6d70942988f229e3d311",
+    "url": "/binder/static/css/main.1dc93acb.chunk.css"
+  },
+  {
+    "revision": "2bd9a39dfa3b12d01a5c",
+    "url": "/binder/static/js/2.3f30c02b.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/binder/static/js/2.3f30c02b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d70942988f229e3d311",
+    "url": "/binder/static/js/main.035300b1.chunk.js"
+  },
+  {
+    "revision": "50e5788d883965f35d6a",
+    "url": "/binder/static/js/runtime-main.e6dadeb0.js"
+  }
+]);
