4cd117e980dc173831481f809493605ac659822a
Pete Redhead <pete@binderpos.com>
2020-10-21 09:29:23 +1300

    Updates

---
+++ precache-manifest.6834fff9311838d4e5b7da4e2ae15eb0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "697fd3b16a7d2f7c28ad8df973e8bdf4",
+    "url": "/binder/index.html"
+  },
+  {
+    "revision": "31e525a6117b6ece04d8",
+    "url": "/binder/static/css/main.1dc93acb.chunk.css"
+  },
+  {
+    "revision": "bb8b64e62b448967c82c",
+    "url": "/binder/static/js/2.829d411f.chunk.js"
+  },
+  {
+    "revision": "2e54b551fdc3988dd651d638460fe789",
+    "url": "/binder/static/js/2.829d411f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31e525a6117b6ece04d8",
+    "url": "/binder/static/js/main.c86ada56.chunk.js"
+  },
+  {
+    "revision": "50e5788d883965f35d6a",
+    "url": "/binder/static/js/runtime-main.e6dadeb0.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/binder/precache-manifest.441894c0d46edd596829ee8a5c1b324e.js"
+  "/binder/precache-manifest.6834fff9311838d4e5b7da4e2ae15eb0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/binder/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
